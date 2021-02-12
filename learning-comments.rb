e308bba6be5e37b952076778de891bbbbae74c60
Jazmin Solorzano <jazminsol@yahoo.com>
2021-02-12 01:21:52 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,9 @@
+#trying a lab 
+puts "Joy to the world"
+puts "All the boys and girls"
+puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+puts "Joy to the world"
+puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
